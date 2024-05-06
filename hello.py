c5af01d666aae92769fa710f6c86ff2af25eabab
shimedh <shimedh@microsoft.com>
2024-05-06 14:22:39 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
+# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World from App Spaces!'
+
+if __name__ == '__main__':
+    app.run()
